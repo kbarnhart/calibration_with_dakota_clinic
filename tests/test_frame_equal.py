6dc565e1edc76e37ea373611e5edf3f11b527cd0
kbarnhart <katy.barnhart@gmail.com>
2019-05-07 14:35:43 -0600

    sort values

--- tests/test_frame_equal.py
+++ tests/test_frame_equal.py
@@ -9,10 +9,10 @@ files = glob.glob("analysis/*.dat")
 @pytest.mark.parametrize("file", files)
 def test_dat_equiv(file):
     df = pd.read_csv(file, engine="python", sep="\s+")
-    
+
     filename = os.path.split(file)[-1]
     other_file = os.path.join(*["tests", "data", filename])
-    
+
     df_test = pd.read_csv(other_file, engine="python", sep="\s+")
-    
-    assert_frame_equal(df, df_test)
+
+    assert_frame_equal(df.sort_values("%eval_id"), df_test.sort_values("%eval_id"))
