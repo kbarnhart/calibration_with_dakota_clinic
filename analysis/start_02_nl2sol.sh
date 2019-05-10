6ccdfd9e826c3d350b944f6e41741b683fa26d41
kbarnhart <katy.barnhart@gmail.com>
2019-05-10 16:23:12 -0600

    write all to .dat

--- analysis/start_02_nl2sol.sh
+++ analysis/start_02_nl2sol.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 dakota -i dakota_02_nl2sol.in -o dakota_02_nl2sol.out -write_restart dakota_02_nl2sol.rst &> dakota_02_nl2sol.log
+dakota_restart_util to_tabular dakota_02_nl2sol.rst dakota_02_nl2sol.dat
